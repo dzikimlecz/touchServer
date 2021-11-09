63b4be81d2014558bd93efe7294bde895a0ae23f
dzikimlecz <j.pul312@gmail.com>
2021-11-09 17:52:03 +0100

    initial commit

---
+++ src/main/java/me/dzikimlecz/touchserver/TouchServerApplication.java
@@ -0,0 +1,13 @@
+package me.dzikimlecz.touchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouchServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dzikimlecz/touchserver/TouchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dzikimlecz.touchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouchServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
