2c2ac8eec02217c1f6af15873fe993204d2d4a01
dzikimlecz <j.pul312@gmail.com>
2021-11-11 19:47:07 +0100

    changed to RestController added exception handlers

--- src/main/java/me/dzikimlecz/touchserver/controller/MessageController.java
+++ src/main/java/me/dzikimlecz/touchserver/controller/MessageController.java
@@ -3,30 +3,44 @@ package me.dzikimlecz.touchserver.controller;
 import me.dzikimlecz.touchserver.model.ElementAlreadyExistException;
 import me.dzikimlecz.touchserver.model.Message;
 import me.dzikimlecz.touchserver.model.UserProfile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import static java.util.stream.Collectors.*;
 import static me.dzikimlecz.touchserver.model.UserProfile.getUsername;
 import static me.dzikimlecz.touchserver.model.UserProfile.parseTag;
+import static org.springframework.http.HttpStatus.*;
 
-@Controller
-@RequestMapping("/touch/msg/")
+@RestController
+@RequestMapping("/touch/msg")
 public class MessageController {
     private final List<Message> mockMessagesSource = new ArrayList<>();
 
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> notfound(NoSuchElementException e) {
+        return new ResponseEntity<>(e.getMessage(), NOT_FOUND);
+    }
+
+    @ExceptionHandler(ElementAlreadyExistException.class)
+    public ResponseEntity<String> notfound(ElementAlreadyExistException e) {
+        return new ResponseEntity<>(e.getMessage(), BAD_REQUEST);
+    }
+
     @GetMapping
-    public List<Message> fetchMessages() {
+    public Collection<Message> fetchMessages() {
         return List.copyOf(mockMessagesSource);
     }
 
     @GetMapping("/{nameTag}")
-    public List<Message> fetchMessagesTo(@PathVariable String nameTag) {
-        final var nameTagArr = nameTag.split("#");
+    public Collection<Message> fetchMessagesTo(@PathVariable String nameTag) {
+        final var nameTagArr = nameTag.split("_");
         final var username = getUsername(nameTagArr);
         final long tag = parseTag(nameTagArr[1]);
         final var profile = UserProfile.of(username, tag);
@@ -37,15 +51,20 @@ public class MessageController {
     }
 
     @PostMapping
+    @ResponseStatus(CREATED)
     public void sendMessage(@RequestBody Message msg) {
         if (mockMessagesSource.contains(msg))
-            throw new ElementAlreadyExistException();
+            throw new ElementAlreadyExistException("Msg: " + msg + "\nalready exists");
         mockMessagesSource.add(msg);
     }
 
     @DeleteMapping
-    public void dropMessage(@RequestBody Message msg) {
-        if(mockMessagesSource.remove(msg)) return;
-        throw new NoSuchElementException();
+    public Message dropMessage(@RequestBody Message msg) {
+        if(mockMessagesSource.remove(msg)) return null;
+        throw new NoSuchElementException("Msg: " + msg + "\n does not exist");
+    }
+
+    protected void clear() {
+        mockMessagesSource.clear();
     }
 }
--- src/main/java/me/dzikimlecz/touchserver/controller/ProfileController.java
+++ src/main/java/me/dzikimlecz/touchserver/controller/ProfileController.java
@@ -3,16 +3,27 @@ package me.dzikimlecz.touchserver.controller;
 
 import me.dzikimlecz.touchserver.model.ElementAlreadyExistException;
 import me.dzikimlecz.touchserver.model.UserProfile;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@Controller
-@RequestMapping("/touch/profiles/")
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping("/touch/profiles")
 public class ProfileController {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> notfound(NoSuchElementException e) {
+        return new ResponseEntity<>(e.getMessage(), NOT_FOUND);
+    }
+
+    @ExceptionHandler(ElementAlreadyExistException.class)
+    public ResponseEntity<String> notfound(ElementAlreadyExistException e) {
+        return new ResponseEntity<>(e.getMessage(), BAD_REQUEST);
+    }
 
     private final List<UserProfile> mockUserSource = new ArrayList<>();
     public ProfileController() { mockUserSource.add(UserProfile.of("username", 1)); }
@@ -24,7 +35,7 @@ public class ProfileController {
 
     @GetMapping("/{nameTag}")
     public UserProfile getProfile(@PathVariable String nameTag) {
-        final var nameTagArr = nameTag.split("#");
+        final var nameTagArr = nameTag.split("_");
         final var username = UserProfile.getUsername(nameTagArr);
         final long tag = UserProfile.parseTag(nameTagArr[1]);
 
@@ -36,6 +47,7 @@ public class ProfileController {
     }
 
     @PostMapping
+    @ResponseStatus(CREATED)
     public void postProfile(@RequestBody UserProfile userProfile) {
         if (mockUserSource.contains(userProfile))
             throw new ElementAlreadyExistException(userProfile.toString() + " already exists.");
@@ -45,10 +57,13 @@ public class ProfileController {
     @DeleteMapping
     public void deleteProfile(@RequestBody UserProfile userProfile) {
         if(mockUserSource.remove(userProfile)) return;
-        throw new NoSuchElementException();
+        throw new NoSuchElementException(String.format("Profile %s \n does not exist", userProfile));
     }
 
-
+    protected void clear() {
+        mockUserSource.clear();
+        mockUserSource.add(UserProfile.of("username", 1));
+    }
 
 
 }
